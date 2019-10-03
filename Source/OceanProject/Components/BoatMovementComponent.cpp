bfa6fc9f45fb1870d9135eb37ac58db7597f1d0e
Szyszkojad666 <rozmarynowski.r@gmail.com>
2019-10-03 22:15:51 +0200

    BP boat buoyancy changes BoatMovementComponent WiP OceanProject binaries

---
+++ Source/OceanProject/Components/BoatMovementComponent.cpp
@@ -0,0 +1,77 @@
+// 2016 - Community based open project
+
+
+#include "BoatMovementComponent.h"
+#include "Runtime/Engine/Classes/GameFramework/Actor.h"
+#include "Components/PrimitiveComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values for this component's properties
+UBoatMovementComponent::UBoatMovementComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UBoatMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+void UBoatMovementComponent::MoveRight(float Value)
+{
+	SteeringThrow = Value;
+}
+
+void UBoatMovementComponent::MoveForward(float Value)
+{
+	Throttle = Value;
+}
+
+// Called every frame
+void UBoatMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	AddDrivingForce(DeltaTime);
+	// ...
+}
+
+FVector UBoatMovementComponent::CalculateAirResistance()
+{
+	float AirResistanceMagnitude = -pow(Velocity.Size(), 2) * DragCoefficient;
+	FVector AirResistanceVector = Velocity.GetSafeNormal() * AirResistanceMagnitude;
+	return AirResistanceVector;
+}
+
+void UBoatMovementComponent::AddDrivingForce(float DeltaTime)
+{
+	FVector Force = GetOwner()->GetActorForwardVector() * Throttle * MaxDrivingForce;
+	Force += CalculateAirResistance();
+	FVector Acceleration = Force / Mass;
+	FVector AccelerationForce = AccelerationForce + Acceleration * DeltaTime;
+
+	UStaticMeshComponent* OwnerMesh = GetOwner()->FindComponentByClass<UStaticMeshComponent>();
+	if (OwnerMesh)
+	{
+		OwnerMesh->AddForceAtLocation(AccelerationForce, ForceLocation, NAME_None);
+		ApplyRotation(DeltaTime, SteeringThrow);
+	}
+}
+
+void UBoatMovementComponent::ApplyRotation(float DeltaTime, float SteeringThrow)
+{
+	float DeltaLocation = FVector::DotProduct(GetOwner()->GetActorForwardVector(), Velocity) * DeltaTime; // Dot Products gives you the proportion of how close one Vector is to another
+	float DTheta = (DeltaLocation / TurningCircleRadius) * SteeringThrow;
+	FQuat DeltaRotation(GetOwner()->GetActorUpVector(), DTheta);
+	GetOwner()->AddActorWorldRotation(DeltaRotation);
+}
+
---
+++ Source/OceanProject/Components/BoatMovementComponent.h
@@ -0,0 +1,61 @@
+// 2016 - Community based open project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "BoatMovementComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class OCEANPROJECT_API UBoatMovementComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UBoatMovementComponent();
+
+	void MoveRight(float Value);
+	void MoveForward(float Value);
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	
+private:
+
+	UPROPERTY(EditAnywhere)
+	FVector ForceLocation;
+	
+	UPROPERTY(EditAnywhere)
+	float Mass = 1000;
+
+	UPROPERTY(EditAnywhere)
+	float MaxDrivingForce = 10000;
+
+	UPROPERTY(EditAnywhere)
+	float MaxRotation = 90;
+
+	UPROPERTY(EditAnywhere)
+	float TurningCircleRadius = 11;
+
+	//How aerodynamic the boat is
+	UPROPERTY(EditAnywhere)
+	float DragCoefficient = 16;
+
+	FVector Velocity;
+
+	float SteeringThrow;
+
+	float Throttle;
+
+	FVector CalculateAirResistance();
+
+	void AddDrivingForce(float DeltaTime);
+
+	void ApplyRotation(float DeltaTime, float SteeringThrow);
+};
